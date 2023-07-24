4889e249453db3de9282b178d9e51ea1121123cb
thotabhargav123 <bhargavthota2002@gmail.com>
2023-07-24 22:41:22 +0530

    changes

--- database/db.js
+++ database/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 // const URL = `mongodb+srv://${username}:${password}@blogapp.jm77a8i.mongodb.net/?retryWrites=true&w=majority`
 
 const Connection = async () => {
-    const URL = `mongodb+srv://${username}:${password}@blogapp.jm77a8i.mongodb.net/?retryWrites=true&w=majority`
-
+    const URL = process.env.URL || `mongodb+srv://${username}:${password}@blogapp.jm77a8i.mongodb.net/?retryWrites=true&w=majority`
     try {
         mongoose.connect(URL, { useNewUrlParser: true });
         mongoose.set('strictQuery', false);
